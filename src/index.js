3b4d909836ec43d0d7d1336d7ebe7d827df27c20
Jarkko Mönkkönen <jamonkko@gmail.com>
2020-11-15 22:18:33 +0200

    bump version

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 /*!
  * @license
- * console-log-server v0.2.0 (https://github.com/jamonkko/console-log-server#readme)
+ * console-log-server v0.2.1 (https://github.com/jamonkko/console-log-server#readme)
  * Copyright 2020 Jarkko Mönkkönen <jamonkko@gmail.com>
  * Licensed under MIT
  */
