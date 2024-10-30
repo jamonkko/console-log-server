6325bfd6910257a2c6d8b11b06ade41a53dc018b
Jarkko Mönkkönen <jamonkko@Jarkko-M2-Air-15.local>
2024-10-30 22:49:22 +0200

    Bump version to 0.3.1

--- dist/index.js
+++ dist/index.js
@@ -23,7 +23,7 @@ function _defineProperty(e, r, t) { return (r = _toPropertyKey(r)) in e ? Object
 function _toPropertyKey(t) { var i = _toPrimitive(t, "string"); return "symbol" == _typeof(i) ? i : i + ""; }
 function _toPrimitive(t, r) { if ("object" != _typeof(t) || !t) return t; var e = t[Symbol.toPrimitive]; if (void 0 !== e) { var i = e.call(t, r || "default"); if ("object" != _typeof(i)) return i; throw new TypeError("@@toPrimitive must return a primitive value."); } return ("string" === r ? String : Number)(t); } /*!
  * @license
- * console-log-server v0.3.0 (https://github.com/jamonkko/console-log-server#readme)
+ * console-log-server v0.3.1 (https://github.com/jamonkko/console-log-server#readme)
  * Copyright 2024 Jarkko Mönkkönen <jamonkko@gmail.com>
  * Licensed under MIT
  */
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 /*!
  * @license
- * console-log-server v0.3.0 (https://github.com/jamonkko/console-log-server#readme)
+ * console-log-server v0.3.1 (https://github.com/jamonkko/console-log-server#readme)
  * Copyright 2024 Jarkko Mönkkönen <jamonkko@gmail.com>
  * Licensed under MIT
  */
